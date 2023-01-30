3297ae24b8d420ea9830b72f730fc84e738ae920
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-01-30 09:57:21 +0530

    added feature and update style

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
+const topLink = document.querySelector(".top-link")
+
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > 300) {
+        topLink.classList.add('show-link');
+        console.log(topLink.classList);
+    } else{
+        topLink.classList.remove('show-link');
+        console.log(topLink.classList);
+    }
+})
