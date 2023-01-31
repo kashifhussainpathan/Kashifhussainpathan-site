5eb6f0ebc0b783e05b9c4ab3bfd0aa7a17e58919
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-01-31 21:15:49 +0530

    Style and Added Functionality

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
 const topLink = document.querySelector(".top-link")
+const bodyEL = document.body;
 
 window.addEventListener("scroll", function () {
     const scrollHeight = window.pageYOffset;
     if (scrollHeight > 300) {
         topLink.classList.add('show-link');
         console.log(topLink.classList);
-    } else{
+    } else {
         topLink.classList.remove('show-link');
         console.log(topLink.classList);
     }
 })
+
+const matchMode = window.matchMedia('(prefers-color-scheme:dark)');
+if (matchMode.matches === true) {
+    bodyEL.classList.add("dark");
+} else {
+    bodyEL.classList.remove("dark")
+}
